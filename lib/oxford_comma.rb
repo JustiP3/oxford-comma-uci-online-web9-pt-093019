b8cef34aaaeae15512e87b1ba285602164e8358b
Justin Phillips <justinphil02@yahoo.com>
2019-10-25 18:34:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,9 +7,10 @@ string = array.join
 elsif array.length == 2
 string = array.join(" and ")
 else
-last_element = array.pop
-string = array.join(", ")
-return "#{string}, and #{last_element}"
+#last_element = array.pop
+#string = array.join(", ")
+#return "#{string}, and #{last_element}"
+array[-1].insert(0, "and ").join(", ")
 end
 
 
